09f96c860827aa3efab80b138f1b3ac6772a2f00
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-07-20 15:25:01 -0700

    transfered code, initial test passes - proving that the connections are good

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const app = require('./lib/app.js');
+const db = require('./lib/db.js');
+
+const url = 'mongodb://localhost:27017/dogs';
+db.connect(url);
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(3000, () => {
+    console.log('server is running on', server.address().port);
+});
