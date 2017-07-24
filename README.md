0d9fc9920609bacc3997005b20580f0b00f7fdf1
Wendy Beck <webeck@gmail.com>
2017-07-24 11:54:19 -0700

    incremental commit - adding notes and some code

---
+++ README.md
@@ -0,0 +1,11 @@
+## First half-stack app
+
+```
+Mongo DB
+
+~~Express~~
+
+~~Angular~~
+
+Node
+```
--- tests/half-stack.tests.js
+++ tests/half-stack.tests.js
@@ -1,13 +1,31 @@
-const app = require('../lib/app.js');
+const app = require('../lib/app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
 
-describe('mongo tests', () => {
+// To pass object ids to mongo:
+// const ObjectID = require('mongodb').ObjectID;
+// // in your code, assuming you have id with string id:
+// const objectId = new ObjectID(id);
+// in Robo 3T: ObjectId(id)
+// in node using mongodb driver: new ObjectID(id)
+
+describe.skip('mongo tests', () => {
   const req = chai.request(app);
 
-  // it('url is GET /resource', done => {}), it('url is POST /resource', done => {}), it('url is GET /resource/:id', done => {}), it('url is DELETE /<resource>/:id', done => {
+  // drop database, create new one
+
+  it('url is GET /resource', done => {
+    return req
+      .get('/people')
+      .query({
+        action : 'moving-up',
+        name : 'jefferson'
+      })
+      .then(res => {});
+  });
+  //  it('url is POST /people', done => {}), it('url is GET /people/:id', done => {}), it('url is DELETE /<people>/:id', done => {
   //   // return { removed: true } or { removed: false }
-  // }), it('url is PUT /<resource>/:id', done => {});
+  // }), it('url is PUT /<people>/:id', done => {});
 });
