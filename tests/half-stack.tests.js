ced9f9c91ebd4609e18512b8964244a1109c3ffa
Wendy Beck <webeck@gmail.com>
2017-07-24 22:36:09 -0700

    logic in place, stuck on weird error

--- tests/half-stack.tests.js
+++ tests/half-stack.tests.js
@@ -2,9 +2,7 @@ const app = require('../lib/app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-const {
- assert
-} = chai.assert;
+const { assert } = chai.assert;
 const ObjectID = require('mongodb').ObjectID;
 const db = require('../lib/db');
 const url = 'mongodb://localhost:27017/halfStack';
@@ -23,18 +21,39 @@ describe.skip('mongo tests', () => {
   beforeEach(() => {
     db.db.dropDatabase();
   });
+
   const req = chai.request(app);
 
+  const people = [
+    { name: 'lucinda', eyes: 'blue' },
+    { name: 'finn', eyes: 'brown' },
+    { name: 'gus', nose: 'large' }
+  ];
+
   // create a new save method here to be shared with all functions
+  it.only('url is POST /people', () => {
+    console.log('1')
+    return Promise.all(people.map((person) => {
+      return req.post('/people')
+        .send(person)
+        .then(res => {
+          console.log('7');
+          console.log(res.body);
+        });
+    }));
+  }),
+    it('url is GET /people', () => {
+      return req.get('/people')
+        .then(res => {
+          console.log(res);
+        });
+    }), it('url is POST /people', () => {
 
-  // drop database, create new one
+    }), it('url is GET /people/:id', () => {
 
-  it.only('url is GET /people', () => {
-    return req.get('/people').then(res => {
-      // console.log(res);
-    });
-  });
-  // it('url is POST /people', done => {}), it('url is GET /people/:id', done => {}), it('url is DELETE /<people>/:id', done => {
-  //   // return { removed: true } or { removed: false }
-  // }), it('url is PUT /<people>/:id', done => {});
+    }), it('url is DELETE /<people>/:id', () => {
+      // return { removed: true } or { removed: false }
+    }), it('url is PUT /<people>/:id', () => {
+
+    })
 });
