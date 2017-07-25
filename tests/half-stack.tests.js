bc61775d2cb177f0b648bbe944853facd88606fb
Wendy Beck <webeck@gmail.com>
2017-07-25 09:52:10 -0700

    all is happening in the right order, save is working

--- tests/half-stack.tests.js
+++ tests/half-stack.tests.js
@@ -2,7 +2,7 @@ const app = require('../lib/app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-const { assert } = chai.assert;
+const assert = chai.assert;
 const ObjectID = require('mongodb').ObjectID;
 const db = require('../lib/db');
 const url = 'mongodb://localhost:27017/halfStack';
@@ -17,23 +17,25 @@ describe('mongo tests', () => {
 
   const req = chai.request(app);
 
-  const people = [
-    { name: 'lucinda', eyes: 'blue' },
-    { name: 'finn', eyes: 'brown' },
-    { name: 'gus', nose: 'large' }
-  ];
-
-  // create a new save method here to be shared with all functions
-  it.only('url is POST /people', () => {
-    console.log('1')
-    return Promise.all(people.map((person) => {
-      return req.post('/people')
+  function save(person) {
+    return new Promise((resolve, reject) => {
+      req.post('/people')
         .send(person)
         .then(res => {
           console.log('7');
-          console.log(res.body);
+          resolve(JSON.parse(res.text));
         });
-    }));
+    });
+  }
+
+  it.only('url is POST /people', () => {
+    console.log('1');
+    const person = { name: 'chuck', feet: 'ginormous' };
+    return save(person)
+      .then(res => {
+        console.log('8');
+        assert.equal('chuck', res.name);
+      });
   }),
     it('url is GET /people', () => {
 
@@ -45,5 +47,5 @@ describe('mongo tests', () => {
       // return { removed: true } or { removed: false }
     }), it('url is PUT /<people>/:id', () => {
 
-    })
+    });
 });
