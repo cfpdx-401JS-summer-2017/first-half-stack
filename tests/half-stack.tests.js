b86406bf8a66fd3a50d271c5903391a40677980b
Wendy Beck <webeck@gmail.com>
2017-09-29 05:05:37 -0700

    adding tests

--- tests/half-stack.tests.js
+++ tests/half-stack.tests.js
@@ -4,87 +4,61 @@ const connect = require('../lib/utils/connect');
 const connection = require('mongoose').connection;
 const req = require('../lib/utils/request');
 const db = require('../lib/utils/db');
-// const seedPeople = require('../testdata/seedPeople');
+const seedPeople = require('./seedPeople')
 
-describe.skip('e2e person tests', () => {
+describe('e2e person tests', () => {
+  let seededPeople = []
+
   before(() => {
     connect();
     db.drop();
   })
-    before(() => {
-      const seedPeople = [
-        {
-          name: 'elizabeth',
-          nose: 'tiny'
-        },
-        {
-          name: 'joann',
-          job: 'web dev'
-        },
-        {
-          name: 'elton',
-          talent: 'piano'
-        },
-        {
-          name: 'oliver',
-          home: 'madrid'
-        },
-        {
-          name: "james",
-          hobby: "collects subway tokens from around the world"
-        }
-      ]
-      seedPeople.map(person => {
-        return req.post('/people').send(person)
-          .then(res => {
-            return
-          })
-      })
-    });
-
 
   it('POST /people', async () => {
     for (let i = 0; i < seedPeople.length; i++) {
-      await req
+      const person = await req
         .post('/people')
         .send(seedPeople[i])
-        .then(user => {
-          assert.hasAnyKeys(user.body, ['code', '_id']);
-        })
-        .catch();
+      seededPeople.push(person.body)
     }
+    assert.equal(seededPeople.length, 6);
   });
   it('GET /people', async () => {
+    // send parameters home, likesRollerCoasters
     const foundPeople = await req.get('/people');
-    assert.lengthOf(foundPeople.body, 2);
+    assert.lengthOf(foundPeople.body, 5);
+    const query = 'likesRollerCoasters=true'
+    // const lRC = await req.get('/people').query({name:'paul'})
+    return req.get('/people').query({likesRollerCoasters:true})
+    .then(results => console.log(47, results.body))
+
+    // console.log(333, lRC)
   }),
-    it('GET /people/:id', async () => {
-      const getOne = await req.get('/people');
-      const id = getOne.body[0]._id;
-      const getOneById = await req.get(`/people/${id}`);
-      assert(getOne.body[0].name, getOneById.body.name);
-    }),
-    it('PUT /people/:id', async () => {
-      const getOne = await req.get('/people');
-      const id = getOne.body[0]._id;
-      const updatedUser = await req
-        .put(`/people/${id}`)
-        .send({
-          name: 'larry jones',
-          likesRollerCoasters: true,
-          heightInInches: 90
-        });
-      assert.equal(updatedUser.body.name, 'larry jones');
-      assert.notEqual(
-        updatedUser.body.likesRollerCoasters,
-        getOne.body[0].likesRollerCoasters
-      );
-    }),
-    it('DELETE /people/:id', async () => {
-      const getOne = await req.get('/people');
-      const id = getOne.body[0]._id;
-      const deleted = await req.delete(`/people/${id}`);
-      assert.doesNotHaveAnyKeys(deleted.body);
-      assert.isEmpty(deleted.body);
-    });
+  it('GET /people/:id', async () => {
+    const id = seededPeople[0]._id;
+    const getOneById = await req.get(`/people/${id}`);
+    assert(seededPeople[0].name, getOneById.body.name);
+  }),
+  it('PUT /people/:id', async () => {
+    const id = seededPeople[4]._id;
+    const updatedUser = await req
+      .put(`/people/${id}`)
+      .send({
+        name: 'larry jones',
+        likesRollerCoasters: true,
+        heightInInches: 90
+      });
+    assert.equal(updatedUser.body.name, 'larry jones');
+    assert.notEqual(
+      updatedUser.body.likesRollerCoasters,
+      seededPeople[2].likesRollerCoasters
+    );
+  }),
+  it('DELETE /people/:id', async () => {
+    const getOne = await req.get('/people');
+    const id = seededPeople[3]._id;
+    const deleted = await req.delete(`/people/${id}`);
+    assert.doesNotHaveAnyKeys(deleted.body);
+    assert.isEmpty(deleted.body);
+  });
 });
