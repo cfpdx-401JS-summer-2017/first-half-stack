b67c316dcf0f6e3da5b39ba1ed4829b30f1044c3
Wendy Beck <webeck@gmail.com>
2017-09-29 01:30:52 -0700

    fixing db stuff

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const http = require('http');
 const app = require('./lib/app');
-const db = require('./lib/db');
 
-const url = 'mongodb://localhost:27017/halfStack';
-db.connect(url);
 
 const server = http.createServer(app);
 const port = 3000;
--- tests/half-stack.tests.js
+++ tests/half-stack.tests.js
@@ -3,65 +3,68 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
-const ObjectID = require('mongodb').ObjectID;
-const db = require('../lib/db');
+const req = chai.request(app);
+const connection = require('../lib/db');
 const url = 'mongodb://localhost:27017/halfStack';
 
-// Database->Collection->Documents
-
 describe('mongo tests', () => {
-  before(() => db.connect(url));
-  beforeEach(() => {
-    db.db.dropDatabase();
+
+  before(() => connection.connect(url));
+  before(() => connection.db.dropDatabase());
+  before(() => {
+    const seedPeople = [
+      {
+        name: 'elizabeth',
+        nose: 'tiny'
+      },
+      {
+        name: 'joann',
+        job: 'web dev'
+      },
+      {
+        name: 'elton',
+        talent: 'piano'
+      },
+      {
+        name: 'oliver',
+        home: 'madrid'
+      },
+      {
+        name: "james",
+        hobby: "collects subway tokens from around the world"
+      }
+    ]
+    seedPeople.map(person => {
+      return req.post('/people').send(person)
+        .then(res => {
+          return
+        })
+    })
   });
 
-  const req = chai.request(app);
 
-  const seedPeople = [
-    { name: 'elizabeth', nose: 'tiny' },
-    { name: 'joann', job: 'web dev' },
-    { name: 'elton', talent: 'piano' },
-    {
-      name: 'oliver', quirks: [
-        { annoying: "clips toenails on transit" },
-        { admirable: "always buys rounds" }
-      ]
-    },
-    { name: "james", hobby: "collects subway tokens from around the world" }
-  ]
 
-  function save(person) {
-    return req.post('/people')
-      .send(person)
-      .then(res => {
-        console.log('7');
-        return res.body;
-      });
-  }
-
-  it('url is POST /people', () => {
-    console.log('1');
-    const person = { feet: 'ginormous', name: 'chuck' };
-    return save(person)
-      .then(res => {
-        console.log('8');
-        assert.equal('chuck', res.name);
-        assert.ok(res._id);
-      });
+  it('url is POST /people', async () => {
+    const person = {
+      feet: 'ginormous',
+      name: 'chuck'
+    };
+    const savedPerson = await req.post('/people').send(person)
+    assert.equal('chuck', savedPerson.body.name);
+    assert.ok(savedPerson.body._id);
   });
-  it.only('url is GET /people', () => {
-    return Promise.all(seedPeople.map((person) => {
-      return save(person)
-    }))
-      .then(res => {
-        return req.get('/people')
-          .then(data => {
-            assert.equal(data.body.length, 5);
-          })
-      });
+  it('url is GET /people', async () => {
+    const getPeople = await req.get('/people')
+    assert.equal(getPeople.body.length, 6);
   });
-  it('url is GET /people/:id', () => {
 
+  it('url is GET /people/:id', async () => {
+
+    const getPeople = await req.get('/people')
+    console.log(88, getPeople.body[0]._id)
+    const id = getPeople.body[0]._id
+    // const getById = await req.get(`/people/${id}`)
+    // console.log(67, getById.body)
   }), it('url is DELETE /<people>/:id', () => {
     // return { removed: true } or { removed: false }
   }), it('url is PUT /<people>/:id', () => {
